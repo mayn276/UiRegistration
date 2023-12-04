cfc279967658c80f7c118d8c1bf80d004ae46d0b
Mayank Sharma <Ms1202864@gmail.com>
2023-12-04 16:46:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val userName = intent.getStringExtra("USER")
+        val textView = findViewById<TextView>(R.id.tvOffer)
+        val message = "$userName ,you will get  free access to all the content for one month "
+        textView.text = message
+
+    }
+}
